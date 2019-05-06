d0cd4eec5559e43b8784f071113acd91f0e01e13
1642morozov <mmorozov98@mail.ru>
2019-05-06 21:12:44 +0300

    s

--- source.cpp
+++ source.cpp
@@ -1,3 +1,56 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+
+struct book
+{
+    string name;        //  nazvanie kn
+    string autor;       //  autor
+    string publishing;  //  izdatel
+    string series;      //  seriya
+    int year;           //  god sozdaniya
+    int page;           //  stranici
+    int binding;        //  pereplet    (1-tverdiy 2-myagkiy)
+    int format;         //  format kn  (4=A4 5=A5 ...)
+    int noc;            //  tirag
+    int n;              //  kol-vo
+};
+
+class baselib           //  abstr fabr
+{
+public:
+    book* b;
+    void fileread ()
+    {
+
+    }
+    void addb ()
+    {
+
+    }
+    void delb ()
+    {
+
+    }
+    void chab ()        // izmenit info o kn
+    {
+
+    }
+    void searchb ()     // poisk kn
+    {
+
+    }
+    void searcha ()     // poisk po autoru
+    {
+
+    }
+    void searchs ()     // poisk po serii
+    {
+
+    }
+    void searchi ()     // poisk po izdatelu
+    {
+
+    }
+
+};
